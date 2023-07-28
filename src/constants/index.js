5fec6214baf7899584df6533c1721f8818b1a682
AlexMuhammad <alexmuhammad@students.amikom.ac.id>
2023-07-29 04:39:10 +0700

    feat: beranda slicing

---
+++ src/constants/index.js
@@ -0,0 +1,32 @@
+const { BerandaImage } = require("@/utils/icon");
+
+export const CONTENT_BERANDA = [
+  {
+    id: 1,
+    title: "Lorem ipsum dolor sit amet consectetur. Lorem ipsum?",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Nisi enim nulla integer lobortis praesent sem. Faucibus risus.",
+    image: BerandaImage,
+  },
+  {
+    id: 2,
+    title: "Lorem ipsum dolor sit amet consectetur. Lorem ipsum?",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Nisi enim nulla integer lobortis praesent sem. Faucibus risus.",
+    image: BerandaImage,
+  },
+  {
+    id: 3,
+    title: "Lorem ipsum dolor sit amet consectetur. Lorem ipsum?",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Nisi enim nulla integer lobortis praesent sem. Faucibus risus.",
+    image: BerandaImage,
+  },
+  {
+    id: 4,
+    title: "Lorem ipsum dolor sit amet consectetur. Lorem ipsum?",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Nisi enim nulla integer lobortis praesent sem. Faucibus risus.",
+    image: BerandaImage,
+  },
+];
--- src/utils/icon.js
+++ src/utils/icon.js
@@ -1,4 +1,23 @@
 import { MdChevronRight } from "react-icons/md";
 import Feans from "@/assets/icons/finease.svg";
+import FeansText from "@/assets/icons/finease-text.svg";
+import Coin from "@/assets/icons/coins.svg";
+import FeansWhite from "@/assets/icons/finease-white.svg";
+import Message from "@/assets/icons/message.svg";
+import Saldo from "@/assets/images/saldo.png";
+import Tunai from "@/assets/images/tunai.png";
+import Riwayat from "@/assets/images/riwayat.png";
+import BerandaImage from "@/assets/images/home-content.png";
 
-export { MdChevronRight, Feans };
+export {
+  MdChevronRight,
+  Feans,
+  Coin,
+  FeansWhite,
+  Message,
+  Saldo,
+  Tunai,
+  Riwayat,
+  FeansText,
+  BerandaImage,
+};
